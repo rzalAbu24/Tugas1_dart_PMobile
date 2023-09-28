2734c6b9e1b83f7e560d813280841d34aca92926
rzalAbu24 <rizalabu024@gmail.com>
2023-09-28 23:30:45 +0800

    Membuat Komentar

---
+++ bin/komentar.dart
@@ -0,0 +1,13 @@
+//Ini adalah contoh komentar satu baris
+
+
+///Komentar yang biasa digunakan untuk dokumentasi
+
+void main() {
+  // Ini adalah variabel
+  var nama = "Abu Rizal";
+  print(nama);
+  /*Komentar lebih
+  var siswa1 = "rizal";
+  dari satu baris */
+}
