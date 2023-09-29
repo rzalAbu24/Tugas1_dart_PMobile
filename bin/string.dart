acd15052017864878e53f8a189a7b9f8ed4a77be
rzalAbu24 <rizalabu024@gmail.com>
2023-09-29 10:18:18 +0800

    edit string

--- bin/string.dart
+++ bin/string.dart
@@ -4,4 +4,23 @@ void main () {
 
   print(namaDepan);
   print(namaBelakang);
+
+  // Kode Expression
+  var namaLengkap = '$namaDepan ${namaBelakang}';
+  print(namaLengkap);
+
+  // Karakter Backslah
+  var text = 'ini adalah \'dart\' \$cool;';
+  print(text);
+
+  // Menggabungkan String
+  var nama1 = namaDepan+namaBelakang;
+  var nama2 = 'Abu' 'Rizal' 'Rizal';
+  print(nama1);
+  print(nama2);
+
+
+  // Multiline String
+  var longString = '''nama saya adalah Abu Rizal dan saya kuliah di STMIK Indonesia Banjarmasin''';
+  print(longString);
 }
