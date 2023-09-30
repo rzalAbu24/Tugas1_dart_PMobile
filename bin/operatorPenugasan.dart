ec8ef571733075658ee3abe9a25f7fa8c4b6ca65
rzalAbu24 <rizalabu024@gmail.com>
2023-09-30 21:06:39 +0800

    Operator Penugasan

---
+++ bin/operatorPenugasan.dart
@@ -0,0 +1,56 @@
+// OPERATOR PENUGASAN
+void main() {
+
+  // +=
+  var a = 12;
+  var b = 11;
+  a += b;
+  print(a);
+
+  // -=
+  var c = 5;
+  var d = 6;
+  c -= d;
+  print(c);
+
+  // *=
+  var e = 10;
+  var f = 15;
+  e *= f;
+  print(e);
+
+  // /=
+  var g = 20.1;
+  var h = 1.5;
+  g /= h;
+  print(g);
+
+  // ~/=
+  var i = 30;
+  var j = 15;
+  i ~/= j;
+  print(i);
+
+  // %=
+  var k = 13;
+  var l = 2;
+  k %= l;
+  print(k);
+
+  // INCREMENT dan DECREMENT
+  var m = 2;
+  ++m;
+  print(m);
+
+  var n = 5;
+  n++;
+  print(n);
+
+  var o = 9;
+  --o;
+  print(o);
+
+  var p = 10;
+  p--;
+  print(p);
+  }
