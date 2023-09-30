eda4da4968b48500a69981b9fab208ee9788d62b
rzalAbu24 <rizalabu024@gmail.com>
2023-09-30 16:47:59 +0800

    Operator Aritmatika

---
+++ bin/OperatorAritmatika.dart
@@ -0,0 +1,34 @@
+// Operator Aritmatika
+void main() {
+  // Penambahan +
+  var penambahan = 5 + 10;
+  print(penambahan);
+
+  // Pengurangan -
+  var pengurangan = 15-2;
+  print(pengurangan);
+
+  // Bilangan negatif
+  var negatif = -2;
+  print(negatif);
+
+  // Bilangan Positif
+  var positif = 4;
+  print(positif);
+
+  // Perkalian *
+  var perkalian = 4 * 6;
+  print(perkalian);
+
+  // Pembagian Hasil Double
+  var pembagian1 = 10 / 4;
+  print(pembagian1);
+
+   // Pembagian Hasil Int
+  var pembagian2 = 10 ~/ 4;
+  print(pembagian2);
+
+  // Sisa Bagi
+  var sisa = 12 % 5;
+  print(sisa);
+}
