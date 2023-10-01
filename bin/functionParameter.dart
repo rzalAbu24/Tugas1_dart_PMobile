51f58a907e9124ea4d600cd006697d869555153d
rzalAbu24 <rizalabu024@gmail.com>
2023-10-02 00:22:44 +0800

    Function Parameter

---
+++ bin/functionParameter.dart
@@ -0,0 +1,9 @@
+// function parameter
+void sayHello(String firstName, lastName) {
+  print('Hello $firstName $lastName');
+}
+
+void main () {
+  sayHello('Abu', 'Rizal');
+  sayHello('Rizal', 'Abu');
+}
