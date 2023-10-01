97c16acf6ee6ff05b2620e26c345c0ab613ea64d
rzalAbu24 <rizalabu024@gmail.com>
2023-10-01 21:06:39 +0800

    IF ELSE

---
+++ bin/ifElse.dart
@@ -0,0 +1,22 @@
+// iff, else if dan else
+void main() {
+  var nilaiSejarah = 80;
+  // if 
+  if (nilaiSejarah >= 95) {
+    print("NILAI : A");
+  // else if
+  } else if (nilaiSejarah >=85) {
+    print("NILAI : B");
+  } else if (nilaiSejarah >=75) {
+    print("NILAI : C");
+  } else if (nilaiSejarah >=65) {
+    print("NILAI : D");
+  } 
+  // else
+  else {
+    print("NILAI E");
+  }
+  
+}
+
+
