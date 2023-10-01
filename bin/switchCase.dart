21e314573ef09e43be0d5a4b7a48a914ad206a28
rzalAbu24 <rizalabu024@gmail.com>
2023-10-01 21:30:07 +0800

    Switch Case

---
+++ bin/switchCase.dart
@@ -0,0 +1,25 @@
+// Switch case
+void main() {
+  var nilai = 'E';
+
+  switch (nilai) {
+    case 'A':
+    print('Anda LULUS dengan sangat baik');
+    break;
+
+    case 'B':
+    print('Anda LULUS dengan baik');
+    break;
+
+    case 'C':
+    print('Anda LULUS');
+    break;
+
+    case 'D':
+    print('Anda TIDAK LULUS');
+    break;
+    
+    default:
+    print('Mungkin anda salah jurusan');
+  }
+}
