768c381ce33a55a65fee04291124d37a93bcc99f
rzalAbu24 <rizalabu024@gmail.com>
2023-10-03 00:02:12 +0800

    Scope

---
+++ bin/scope.dart
@@ -0,0 +1,12 @@
+// scope
+void main() {
+  var nama = 'Abu Rizal';
+
+  void sayGoodBye() {
+    var goodBye = 'Good Bye $nama';
+    print('Good Bye');
+    print(goodBye);
+  }
+  sayGoodBye();
+  // print(goodBye); ERROR TIDAK BISA DIAKSES
+}
