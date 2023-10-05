0ad0cbba9254f95a1f6ed3e57ac0105902874586
rzalAbu24 <rizalabu024@gmail.com>
2023-10-05 23:14:12 +0800

    Object

---
+++ bin/oop/object.dart
@@ -0,0 +1,11 @@
+// Membuat Object
+class MahasiswaStmik {
+  String? nama;
+}
+void main() {
+  var mhsStmik1 = MahasiswaStmik();
+  print(mhsStmik1.nama);
+      //Atau 
+  MahasiswaStmik mhsStmik2= MahasiswaStmik();
+  print(mhsStmik2);
+}
