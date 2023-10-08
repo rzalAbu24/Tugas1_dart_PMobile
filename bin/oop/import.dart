7c2e7fe90d0d85a5219251825c32a2012b3c7e8c
rzalAbu24 <rizalabu024@gmail.com>
2023-10-08 23:03:25 +0800

    Import

---
+++ bin/oop/import.dart
@@ -0,0 +1,9 @@
+// Import
+import 'field.dart';
+
+void main() {
+  var mhs = MahasiswaStmik();
+  mhs.nama = "Abu Rizal";
+  mhs.alamat = 'Jl.Banua Anyar';
+  print("Hallo nama saya ${mhs.nama}, saya tinggal di ${mhs.alamat}");
+}
