c48d689414ee0499c9e8f96c01470b3bf68f6384
rzalAbu24 <rizalabu024@gmail.com>
2023-10-09 15:38:05 +0800

    Abstract class

---
+++ bin/oop/abstract_class.dart
@@ -0,0 +1,8 @@
+import 'location.dart';
+
+void main() {
+  
+  var city = City("Buntok");
+  // var location = Location(); //ERROR KARENA ABSTRACT CLASS TIDAK BISA DIBUAT SEBAGI OBJECT SECARA LANGSUNG
+  print(city);
+}
